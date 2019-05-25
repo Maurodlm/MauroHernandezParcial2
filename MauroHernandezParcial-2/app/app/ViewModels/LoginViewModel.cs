30c9ec6bb872e26be792684671795cd84938b929
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-05-25 13:13:03 -0400

    commit del programa hecho

---
+++ MauroHernandezParcial-2/app/app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,94 @@
+﻿
+namespace app.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Services;
+    using Xamarin.Forms;
+    //using Models;
+    using Views;
+
+    public class LoginViewModel : BaseViewModel
+    {
+        #region Attributes
+        string notes;
+        bool isrunning;
+        bool isenabled;
+
+        public LoginViewModel()
+        {
+        }
+        #endregion
+
+        #region Properties
+        public string Notes
+        {
+            get
+            {
+                return this.notes;
+            }
+            set
+            {
+                SetValue(ref this.notes, value);
+            }
+        }
+
+        private void SetValue(ref string notes, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning, value);
+            }
+
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isenabled;
+            }
+            set
+            {
+                SetValue(ref this.isenabled, value);
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(Notes))
+            {
+                await App.Current.MainPage.DisplayAlert("Notes empty",
+                                "Please enter your note",
+                                "Accept");
+                return;
+            }
+
+
+            IsRunning = true;
+            IsEnabled = false;
+
+        }
+
+        }
+    #endregion
+}
---
+++ MauroHernandezParcial-2/app/app/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace app.ViewModels
+{
+    //using Models;
+
+    public class MainViewModel
+    {
+
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance; public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+            return instance;
+        }
+        #endregion
+    }
+}
